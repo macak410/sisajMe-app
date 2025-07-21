a1e919816a149f3334af0d9267a8721ec458eff6
macak410 <macak410@gmail.com>
2025-07-21 12:03:14 +0200

    fix homepage for mobile

--- app/(root)/account/page.tsx
+++ app/(root)/account/page.tsx
@@ -10,30 +10,35 @@ const AccountPage = async () => {
   const loggedUser = await getLoggedInUser();
 
   return (
-    <div className="flex gap-16">
-      <div>
-        <h1 className="text-2xl font-semibold mb-5">Postavke Računa</h1>
-        <AccountNavigation />
-      </div>
+    <div className="w-full max-w-7xl px-4 sm:px-8 mx-auto py-6">
+      <h1 className="text-2xl font-semibold mb-5 sm:mb-8 text-center sm:text-left">
+        Postavke računa
+      </h1>
 
-      <div className="space-y-5">
-        {loggedUser && (
-          <>
-            <h3 className="heading-h1">
-              Dobrodošli, {loggedUser.name.split(" ").at(0)} 👋
-            </h3>
-            <p className="text-textGray-500 mt-5 mb-8 text-lg font-medium">
-              Vaša savršena frizura u samo nekoliko klikova. Zakažite svoj
-              termin u par sekundi!
-            </p>
-            <Button
-              href={`/customers/${loggedUser.$id}/new-appointment`}
-              className="self-start"
-            >
-              Zakažite termin
-            </Button>
-          </>
-        )}
+      <div className="flex flex-col sm:flex-row gap-8 sm:gap-16">
+        <div className="w-full sm:w-1/3">
+          <AccountNavigation />
+        </div>
+
+        <div className="w-full sm:w-2/3 space-y-5">
+          {loggedUser && (
+            <>
+              <h3 className="heading-h1 text-center sm:text-left">
+                Dobrodošli, {loggedUser.name.split(" ").at(0)} 👋
+              </h3>
+              <p className="text-textGray-500 mt-3 mb-6 text-lg font-medium text-center sm:text-left">
+                Vaša savršena frizura u samo nekoliko klikova. Zakažite svoj
+                termin u par sekundi!
+              </p>
+              <Button
+                href={`/customers/${loggedUser.$id}/new-appointment`}
+                className="w-full sm:w-auto"
+              >
+                Zakažite termin
+              </Button>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
